f579268730cc8b52ae5cf7c807f6dc7c20d51ccb
Joinas <jgoldhammer@outlook.de>
2024-08-08 15:44:30 +0200

    Add module-component

--- ggfit/src/pages/index.tsx
+++ ggfit/src/pages/index.tsx
@@ -1,27 +1,32 @@
+import * as React from 'react';
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import BasicModal from "@/pages/modal";
+import ButtonAppBar from "@/pages/appbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>GGFit</title>
-      </Head>
-      <main className={`${styles.main} ${inter.className}`}>
-        <div>
-          <h1>
-            HELLOOO
-          </h1>
-          This is my new site.
-          <h3>
-            Pretty cool huh?
-          </h3>
-        </div>
-      </main>
-    </>
-  );
+    return (
+        <React.Fragment>
+            <ButtonAppBar/>
+            <Head>
+                <title> GGFit </title>
+                <meta name="description" content="A BRAND NEW FITNESS APP" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+            </Head>
+            <main className={`${styles.main} ${inter.className}`}>
+                <p className={styles.description}>
+                    Become the best version of yourself - RIGHT NOW!
+                </p>
+                <p>
+                    <BasicModal/>
+                </p>
+            </main>
+
+
+        </React.Fragment>
+
+    );
 }
