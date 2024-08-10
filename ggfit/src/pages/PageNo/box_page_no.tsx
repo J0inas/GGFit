7f60ba2fef7e7798a6675af2c612b8df6bcb846a
Lukas Friesecke <lf_accounts@tutanota.com>
2024-08-10 17:19:34 +0200

    Add simple buttons

--- ggfit/src/pages/PageNo/box_page_no.tsx
+++ ggfit/src/pages/PageNo/box_page_no.tsx
@@ -1,22 +1,99 @@
 import * as React from 'react'
 import Box from "@mui/material/Box"
-import { useTheme } from "@mui/material/styles";
+import Grid from "@mui/material/Grid"
+import Stack from "@mui/system/Stack"
+import Button from '@mui/material/Button';
+
+import { useTheme, createTheme, ThemeProvider } from "@mui/material/styles";
+
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 
-// Styling der Box:
-const style_box = {
+// Styling der Main Box:
+const style_box_data = {
     width: "100%",
-    height: "100%",
+    height: 520,
     bgcolor: "#404040"
 }
 
-
-// Aufbau der Box:
-export default function BoxPageNo() {
+// Aufbau der Main Box:
+function BoxData() {
     const theme = useTheme();
 
     return (
-        <Box sx={style_box}>
+        <Box sx={style_box_data}>
+
         </Box>
+    )
+}
+
+
+
+// Styling der Footer Box:
+const style_box_footer = {
+    width: "100%",
+    height: 40,
+}
+
+// Augment the palette to include an white color
+declare module '@mui/material/styles' {
+    interface Palette {
+        white: Palette['primary'];
+    }
+
+    interface PaletteOptions {
+        white?: PaletteOptions['primary'];
+    }
+}
+
+// Update the Button's color options to include an ochre option
+declare module '@mui/material/Button' {
+    interface ButtonPropsColorOverrides {
+        white: true;
+    }
+}
+
+const footer_theme = createTheme({
+    palette: {
+        white: {
+            main: '#F0F0F0',
+        },
+    },
+});
+
+// Aufbau der Footer Box:
+function BoxFooter() {
+
+    return (
+        <Box sx={style_box_footer}>
+            <ThemeProvider theme={footer_theme}>
+                <Grid
+                    container
+                    direction="row"
+                    justifyContent="space-between"
+                    alignItems="center"
+                >
+                    <Button variant="outlined" color="white"><ArrowBackIosNewIcon/></Button>
+                    <Button variant="outlined" color="white"><ArrowForwardIosIcon/></Button>
+                </Grid>
+            </ThemeProvider>
+
+        </Box>
+    )
+}
+
+
+
+// Aufbau der Box:
+export default function BoxProgressBar() {
+    const theme = useTheme();
+
+    return (
+        <Stack spacing={3}>
+            <Box sx={{height: 10}}></Box>
+            <BoxData></BoxData>
+            <BoxFooter></BoxFooter>
+        </Stack>
     );
 }
--- ggfit/src/pages/ProgressBar/box_progress_bar.tsx
+++ ggfit/src/pages/ProgressBar/box_progress_bar.tsx
@@ -1,16 +1,15 @@
 import * as React from 'react'
 import { styled } from '@mui/material/styles'
 import Box from "@mui/material/Box"
+import Grid from "@mui/material/Grid"
+import Stack from "@mui/system/Stack"
+import Button from '@mui/material/Button';
+
 import LinearProgress, {linearProgressClasses } from "@mui/material/LinearProgress";
-import { useTheme } from "@mui/material/styles";
+import { useTheme, createTheme, ThemeProvider } from "@mui/material/styles";
 
-
-// Styling der Box:
-const style_box = {
-    width: "100%",
-    height: "100%",
-    bgcolor: "#404040"
-}
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 
 // Styling der Progress Bar:
@@ -22,8 +21,89 @@ const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
     },
     [`& .${linearProgressClasses.bar}`]: {
         borderRadius: 4,
-        backgroundColor: theme.palette.grey[400],
+        backgroundColor: theme.palette.grey[300],
     },
+}));
+
+
+// Styling der Main Box:
+const style_box_data = {
+    width: "100%",
+    height: 520,
+    bgcolor: "#404040"
+}
+
+// Aufbau der Main Box:
+function BoxData() {
+    const theme = useTheme();
+
+    return (
+        <Box sx={style_box_data}>
+
+        </Box>
+    )
+}
+
+
+// Styling der Footer Box:
+const style_box_footer = {
+    width: "100%",
+    height: 40,
+}
+
+// Augment the palette to include an white color
+declare module '@mui/material/styles' {
+    interface Palette {
+        white: Palette['primary'];
+    }
+
+    interface PaletteOptions {
+        white?: PaletteOptions['primary'];
+    }
+}
+
+// Update the Button's color options to include an ochre option
+declare module '@mui/material/Button' {
+    interface ButtonPropsColorOverrides {
+        white: true;
+    }
+}
+
+const footer_theme = createTheme({
+    palette: {
+        white: {
+            main: '#F0F0F0',
+        },
+    },
+});
+
+// Aufbau der Footer Box:
+function BoxFooter() {
+
+    return (
+        <Box sx={style_box_footer}>
+            <ThemeProvider theme={footer_theme}>
+                <Grid
+                    container
+                    direction="row"
+                    justifyContent="space-between"
+                    alignItems="center"
+                >
+                    <Button variant="outlined" color="white"><ArrowBackIosNewIcon/></Button>
+                    <Button variant="outlined" color="white"><ArrowForwardIosIcon/></Button>
+                </Grid>
+            </ThemeProvider>
+
+        </Box>
+    )
+}
+
+
+const Item = styled('div')(({ theme }) => ({
+    backgroundColor: '#D9D9D9',
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    borderRadius: 4,
 }));
 
 
@@ -32,8 +112,10 @@ export default function BoxProgressBar() {
     const theme = useTheme();
 
     return (
-        <Box sx={style_box}>
-            <BorderLinearProgress variant="determinate" value={50} />
-        </Box>
+        <Stack spacing={3}>
+            <Box sx={{height: 10}}><BorderLinearProgress variant="determinate" value={50}/></Box>
+            <BoxData></BoxData>
+            <BoxFooter></BoxFooter>
+        </Stack>
     );
 }
