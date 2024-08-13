c84079e90118f496514ee1abe7e8f75e6d4fdf5c
Lukas Friesecke <lf_accounts@tutanota.com>
2024-08-13 14:47:55 +0200

    Add first registration page

--- ggfit/src/pages/ProgressBar/box_progress_bar.tsx
+++ ggfit/src/pages/ProgressBar/box_progress_bar.tsx
@@ -4,7 +4,7 @@ import Box from "@mui/material/Box"
 import Grid from "@mui/material/Grid"
 import Stack from "@mui/system/Stack"
 import Button from '@mui/material/Button'
-import Slide from "@mui/material/Slide"
+import TextField from '@mui/material/TextField'
 import Fade from "@mui/material/Fade"
 
 import LinearProgress, {linearProgressClasses } from "@mui/material/LinearProgress";
@@ -35,17 +35,6 @@ const style_box_data = {
     bgcolor: "#404040"
 }
 
-// Aufbau der Main Box:
-function BoxData() {
-    const theme = useTheme();
-
-    return (
-        <Box sx={style_box_data}>
-
-        </Box>
-    )
-}
-
 
 // Styling der Footer Box:
 const style_box_footer = {
@@ -53,7 +42,7 @@ const style_box_footer = {
     height: 40,
 }
 
-// Augment the palette to include an white color
+// Augment the palette to include a white color
 declare module '@mui/material/styles' {
     interface Palette {
         white: Palette['primary'];
@@ -80,12 +69,36 @@ const footer_theme = createTheme({
 });
 
 
-const Item = styled('div')(({ theme }) => ({
-    backgroundColor: '#D9D9D9',
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    borderRadius: 4,
-}));
+const sx_textbox = {
+    // Root class for the input field
+    "& .MuiOutlinedInput-root": {
+        color: "#707070",                // ???
+        fontFamily: "Arial",
+        // Class for the border around the input field
+        "& .MuiOutlinedInput-notchedOutline": {
+            borderColor: "#707070",      // Default Farbe der Border
+            borderWidth: "1px",
+        },
+        "&.Mui-focused": {
+            "& .MuiOutlinedInput-notchedOutline": {
+                borderColor: "#909090",  // Clicked Farbe der Border
+                borderWidth: "1px",
+            },
+        },
+        "&:hover:not(.Mui-focused)": {
+            "& .MuiOutlinedInput-notchedOutline": {
+                borderColor: "#909090",  // Hovered Farbe der Border
+            },
+        },
+    },
+    // Class for the label of the input field ???
+    "& .MuiInputLabel-outlined": {
+        color: "#ff0000",
+        "&.Mui-focused": {
+            color: "#ff0000",
+        },
+    },
+}
 
 
 // Aufbau der Box:
@@ -98,6 +111,7 @@ export default function BoxProgressBar() {
         setProgress((prevProgress) => Math.max(Math.min(prevProgress + delta, 100), 0));
     };
 
+
     // Sliding der Anmeldung:
     const [boxNumber, setBoxNumber] = React.useState(0);
     const increaseBoxNumber = () => {
@@ -108,20 +122,94 @@ export default function BoxProgressBar() {
     }
 
 
+    // Ausgefüllte Daten:
+    const [firstName, setFirstName] = React.useState('');
+    const [lastName, setLastName] = React.useState('');
+    const [firstMail, setFirstMail] = React.useState('');
+    const [lastMail, setLastMail] = React.useState('');
+    const handleFirstNameChange = (event: { target: { value: React.SetStateAction<string> } }) => {
+        setFirstName(event.target.value);
+    }
+    const handleLastNameChange = (event: { target: { value: React.SetStateAction<string> } }) => {
+        setLastName(event.target.value);
+    }
+    const handleFirstMailChange = (event: { target: { value: React.SetStateAction<string> } }) => {
+        setFirstMail(event.target.value);
+    }
+    const handleLastMailChange = (event: { target: { value: React.SetStateAction<string> } }) => {
+        setLastMail(event.target.value);
+    }
+
+
     // Aufbau des Formulars:
     return (
         <Stack spacing={3}>
             <Box sx={{height: 10}}><BorderLinearProgress variant="determinate" value={progress}/></Box>
 
-            <Box sx={{ width: 200, height: 200, overflow: 'hidden' }}>
+            <Box sx={{ width: "100%", height: 520, overflow: 'hidden' }}>
+
+
                 <Fade in={boxNumber === 0} mountOnEnter unmountOnExit>
-                    <Box sx={{ width: 200, height: 200, bgcolor: 'blue' }} />
+                    <Box sx={{width: "100%", height: "100%"}}>
+                        <Stack spacing={3} direction="column">
+                            <Box sx={{width: "100%", height: 50}}/>
+                            <Stack spacing={3} direction="row">
+                                <TextField id="first-name"
+                                           label="Vorname"
+                                           variant="outlined"
+                                           fullWidth={true}
+                                           required={true}
+                                           value={firstName}
+                                           onChange={handleFirstNameChange}
+                                           InputProps={{style: { color: 'white' }}}
+                                           InputLabelProps={{style: { color: 'white' }}}
+                                           sx={sx_textbox}
+                                />
+                                <TextField id="last-name"
+                                           label="Nachname"
+                                           variant="outlined"
+                                           fullWidth={true}
+                                           required={true}
+                                           value={lastName}
+                                           onChange={handleLastNameChange}
+                                           InputProps={{style: { color: 'white' }}}
+                                           InputLabelProps={{style: { color: 'white' }}}
+                                           sx={sx_textbox}
+                                />
+                            </Stack>
+                            <Box sx={{width: "100%", height: 50}} />
+                            <TextField id="mail-first"
+                                       label="E-Mail"
+                                       variant="outlined"
+                                       fullWidth={true}
+                                       required={true}
+                                       value={firstMail}
+                                       onChange={handleFirstMailChange}
+                                       InputProps={{style: { color: 'white' }}}
+                                       InputLabelProps={{style: { color: 'white' }}}
+                                       sx={sx_textbox}
+                            />
+                            <TextField id="mail-last"
+                                       label="Bestätigung E-Mail"
+                                       variant="outlined"
+                                       fullWidth={true}
+                                       required={true}
+                                       value={lastMail}
+                                       onChange={handleLastMailChange}
+                                       InputProps={{style: { color: 'white' }}}
+                                       InputLabelProps={{style: { color: 'white' }}}
+                                       sx={sx_textbox}
+                            />
+                        </Stack>
+                    </Box>
+
+
                 </Fade>
                 <Fade in={boxNumber === 1} mountOnEnter unmountOnExit>
-                    <Box sx={{ width: 200, height: 200, bgcolor: 'green' }} />
+                    <Box sx={{ width: "100%", height: "100%", bgcolor: 'green' }} />
                 </Fade>
                 <Fade in={boxNumber === 2} mountOnEnter unmountOnExit>
-                    <Box sx={{ width: 200, height: 200, bgcolor: 'gray' }} />
+                    <Box sx={{ width: "100%", height: "100%", bgcolor: 'gray' }} />
                 </Fade>
             </Box>
 
